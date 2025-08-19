4f7c8f5f30dc6981618056edf15510947b647e11
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-08-19 20:47:01 +0200

    fjern kommentarer

--- Oblig/H25/Oblig 1/Oppgave 2/app.js
+++ Oblig/H25/Oblig 1/Oppgave 2/app.js
@@ -2,6 +2,6 @@ import { initCurrency }  from "./features/currency.js";
 import { initCountries } from "./features/countries.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-  initCurrency();   // Step 1–4
-  initCountries();  // Step 5–6
+  initCurrency();
+  initCountries();
 });
