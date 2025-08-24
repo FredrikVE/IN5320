fdc964476f1e1d38624a1d90be19bbba3282c1f2
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-08-24 11:35:16 +0200

    legg inn div fra kurs

---
+++ Nettkurs/JavaScriptKurs/Types/Strings/wisdome.js
@@ -0,0 +1,9 @@
+
+
+function wisdome() {
+    wisdom_string = "One learns from books and example only that certain things can be done. Actual learning requires that you do those things."
+    return wisdom_string;
+};
+
+
+console.log(wisdome());
