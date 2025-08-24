ca5769e9bdb36f95db65fc0ad51c0f35c2742587
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-08-24 19:50:22 +0200

    bytt til module.export syntaks for å slippe feilmelding i terminal

--- Nettkurs/JavaScriptKurs/Functions/fizzBuzz.js
+++ Nettkurs/JavaScriptKurs/Functions/fizzBuzz.js
@@ -1,6 +1,16 @@
-export default function fizzBuzz(number) {
+function fizzBuzz(number) {
   if (number % 3 === 0 && number % 5 === 0) return "FizzBuzz";
   if (number % 3 === 0) return "Fizz";
   if (number % 5 === 0) return "Buzz";
   return String(number);
 }
+module.exports = fizzBuzz;
+
+
+function main() {
+    for (let i = 0; i <= 20; i++) {
+        console.log(fizzBuzz(i));
+    }
+}
+
+main();
