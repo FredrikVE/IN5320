c6a84c6b9fe180857773c770aa2c01c131869c52
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-08-25 19:57:13 +0200

    legg inn cache i useDataElements()

--- Oblig/H25/Oblig3/src/components/DatasetDetailsView.jsx
+++ Oblig/H25/Oblig3/src/components/DatasetDetailsView.jsx
@@ -1,4 +1,4 @@
-//src/components/DatasetDetailsView.jsx
+// src/components/DatasetDetailsView.jsx
 import { NoticeBox, CircularLoader } from "@dhis2/ui"
 import { useDataElements } from "../hooks/useDataElements"
 import DatasetMetadataTable from "./DatasetMetadataTable"
@@ -6,26 +6,18 @@ import DataElementsTable from "./DataElementsTable"
 
 export default function DatasetDetailsView({ dataset }) {
     if (!dataset) return null
-
-    const { loading, error, elements } = useDataElements(dataset.id)
+    const { loading, error, elements, hasData } = useDataElements(dataset.id)
 
     return (
         <div>
             <h2 style={{ margin: "0 0 12px" }}>{dataset.displayName}</h2>
-
-            {/* Metadata */}
             <DatasetMetadataTable dataset={dataset} />
 
-            {/* Elements */}
             <h3 style={{ margin: "16px 0 8px" }}>Data elements in dataset</h3>
             {error && <NoticeBox error title="Error">{error.message}</NoticeBox>}
             {loading && <CircularLoader />}
-            {!loading && !elements.length && (
-                <NoticeBox>Dataset has no data elements.</NoticeBox>
-            )}
-            {!loading && elements.length > 0 && (
-                <DataElementsTable elements={elements} />
-            )}
+            {!loading && !hasData && <NoticeBox>Dataset has no data elements.</NoticeBox>}
+            {hasData && <DataElementsTable elements={elements} />}
         </div>
     )
 }
