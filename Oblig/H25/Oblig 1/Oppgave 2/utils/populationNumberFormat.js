f5d67e61035c4844ef65f7c3f8a31cb0138fd4ef
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-09-02 20:12:39 +0200

    kommenter bedre

--- Oblig/H25/Oblig 1/Oppgave 2/utils/populationNumberFormat.js
+++ Oblig/H25/Oblig 1/Oppgave 2/utils/populationNumberFormat.js
@@ -1,9 +1,4 @@
-/**
- * Formatterer et befolkningstall for visning (norsk locale).
- * - Runder til heltall
- * - Bruker tusenskilletegn iht. "nb-NO"
- */
-export function formatPopulationNumber(n) {
-  const value = Math.round(Number(n));
-  return value.toLocaleString("nb-NO");
+export function formatPopulationNumber(inputNumber) {
+  const value = Math.round(Number(inputNumber)); // Runder til heltall
+  return value.toLocaleString("nb-NO"); //Returnerer opprundet heltall med tusenskilletegn
 }
