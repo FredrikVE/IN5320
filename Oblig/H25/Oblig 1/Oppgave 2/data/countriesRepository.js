ea9c04052b06ec2f1a920f7ad6b54bbe424b55e4
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-09-03 15:50:34 +0200

    -

--- Oblig/H25/Oblig 1/Oppgave 2/data/countriesRepository.js
+++ Oblig/H25/Oblig 1/Oppgave 2/data/countriesRepository.js
@@ -6,14 +6,14 @@ export class CountriesRepository {
     this.datasource = datasource;
   }
 
-  //Metode som henter, behandler og returnerer informasjon om landets navn og folketall
+  // Metode som henter, behandler og returnerer informasjon om landets navn og folketall
   async  getCountryData(countryName) {
     const query = String(countryName).trim();
     const population = await this.datasource.fetchPopTodayTomorrowRaw(query);
     const populationArray = population?.total_population;
     
-    //hvis datastrukutren har lengde mindre enn to, betyr det at landet ikke finnes.
-    //antar at alle lande som finnes har et today og et tomorrow-element
+    // Hvis datastrukutren har lengde mindre enn to, betyr det at landet ikke finnes.
+    // Antar at alle lande som finnes har et today og et tomorrow-element
     if (!Array.isArray(populationArray) || populationArray.length < 2) {
       throw new Error("COUNTRY_NOT_SUPPORTED"); // returnerer tom/feil ved ukjent land
     }
