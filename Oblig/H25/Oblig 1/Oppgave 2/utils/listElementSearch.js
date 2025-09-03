3b99ce5fad095b5387e76e8211dff9e441fa6885
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-09-03 15:55:02 +0200

    kommenter searchElement.js

--- Oblig/H25/Oblig 1/Oppgave 2/utils/listElementSearch.js
+++ Oblig/H25/Oblig 1/Oppgave 2/utils/listElementSearch.js
@@ -1,13 +1,14 @@
 export function listElementSearch(inputList, searchInput) {
-  const query = searchInput.trim().toLowerCase();
-  const result = [];
-
+  const result = [];                                  // tom liste for resultater
+  const query = searchInput.trim().toLowerCase();     // trimmer input for whitespace og konverterer til lowerCase
+  
+  // Løper igjennom elementene i inputLista for å sjekke om søke-elementet finnes
   for (const i of inputList) {
-    if (i.toLowerCase().startsWith(query)) {
-      result.push(i);
+    if (i.toLowerCase().startsWith(query)) {    // Hvis inputlisteelement starter med samme som query
+      result.push(i);                           // så legges elementet til i resultatlista
     }
   }
-  return result;
+  return result;                // returner resultatlista
 }
 
 
