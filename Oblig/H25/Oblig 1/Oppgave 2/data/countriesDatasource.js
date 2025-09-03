1d4d9f19fe93073619ea3f639411ac9a9cbb51d5
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-09-03 15:50:14 +0200

    -

--- Oblig/H25/Oblig 1/Oppgave 2/data/countriesDatasource.js
+++ Oblig/H25/Oblig 1/Oppgave 2/data/countriesDatasource.js
@@ -3,7 +3,7 @@ const BASE_URL = "https://d6wn6bmjj722w.population.io/1.0/"
 
 export class CountriesDatasource {
 
-  /** Hent JSON fra API, returner null ved feil */
+  /// Hent JSON fra API, returner null ved feil
   async getAPIRequest(path) {
     const request = await fetch(BASE_URL + path);
     
@@ -14,7 +14,7 @@ export class CountriesDatasource {
     return request.json();
   }
   
-  /** hent dagens og morgendagens populasjon for et land */
+  // hent dagens og morgendagens populasjon for et land
   fetchPopTodayTomorrowRaw(countryName) {
     const name = encodeURIComponent(countryName);
     const result = this.getAPIRequest(`population/${name}/today-and-tomorrow/?format=json`);
