f49c57d29a923489da36aba08faff93c0ae66066
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-09-08 20:02:04 +0200

    style spill

--- Nettkurs/React tutorial/Practice projects/tick_tack_toe/src/components/Square.jsx
+++ Nettkurs/React tutorial/Practice projects/tick_tack_toe/src/components/Square.jsx
@@ -1,7 +1,17 @@
 //src/components/Square.jsx
+import "../styles/Squares.css";
+
 export default function Square( props ) {
+    let squareClass = "square";
+    if (props.value === "X") {
+        squareClass += " square-x";
+    } 
+    else if (props.value === "O") {
+        squareClass += " square-o";
+    }
+
     return (
-        <button className="square" onClick={props.onSquareClick}>
+        <button className= {squareClass} onClick={props.onSquareClick}>
         {props.value}
         </button>
     );
