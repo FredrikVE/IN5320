21b5ec8fa2476cd921b63e5a4183686453c8e925
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-09-08 12:34:00 +0200

    lag usestate klasse som komponent

--- Nettkurs/React tutorial/usestateclass/src/App.js
+++ Nettkurs/React tutorial/usestateclass/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
+
+  changeCount(amount) {
+    this.setState(prevState => {
+      return { count: prevState.count + amount }
+    })
+  }
+
+  resetCount() {
+    this.setState({ count: 0 })
+  }
+
+  render() {
+    return (
+      <>
+        <div className="app-container">
+          <div className="counter-container">
+            <button id="increase-btn" onClick={() => this.changeCount(1)}>+</button>
+            <span id="count-value">{this.state.count}</span>
+            <button id="decrement-btn" onClick={() => this.changeCount(-1)}>-</button>
+          </div>
+          
+          <button className="reset-btn" onClick={() => this.resetCount()}>Reset</button>
+
+        </div>
+        
+      </>
+    )
+  }
 }
 
-export default App;
+export default Counter;
