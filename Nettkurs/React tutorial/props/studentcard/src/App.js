eaecdf7e7f9613afb75b481a4b6a387ccc0cd46f
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-09-08 15:48:36 +0200

    formater tekst i student-card

--- Nettkurs/React tutorial/props/studentcard/src/App.js
+++ Nettkurs/React tutorial/props/studentcard/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="app-container">
       <Student name="Bob" age="30" isStudent={false}/>
-      <Student name="Alcie" age="27" isStudent={true}/>
+      <Student name="Alice" age="27" isStudent={true}/>
       <Student name="Kåre" age="29" isStudent={true}/>
 
     </div>
--- Nettkurs/React tutorial/props/studentcard/src/components/Student.jsx
+++ Nettkurs/React tutorial/props/studentcard/src/components/Student.jsx
@@ -3,9 +3,21 @@ import "../styles/student.css"
 export default function Student(props) {
     return(
         <div className="student">
-            <p>Name: {props.name} </p>
-            <p>Age: {props.age} </p>
-            <p>Student: {props.isStudent ? "Yes" : "No"} </p>
+
+            <p>
+                <span className="label">Name: </span>
+                <span>{props.name} </span>
+            </p>
+
+            <p>
+                <span className="label">Age:</span>
+                <span>{props.age}</span>
+            </p>
+
+            <p>
+                <span className="label">Student: </span>
+                <span>{props.isStudent ? "Yes" : "No"}</span> 
+            </p>
         </div>
     );
 }
