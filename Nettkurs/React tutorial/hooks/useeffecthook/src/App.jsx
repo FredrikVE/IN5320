c9042b1de78f78883e1f1b05daa25938f64f8c12
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-09-12 10:35:46 +0200

    init nytt prosjekt

--- Nettkurs/React tutorial/hooks/useeffecthook/src/App.jsx
+++ Nettkurs/React tutorial/hooks/useeffecthook/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import MyComponent from './components/MyComponent'
 import './App.css'
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <MyComponent/>
