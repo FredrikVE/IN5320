39952d5dd8595d1cb585b17d7241a96687afa35c
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-09-17 16:46:26 +0200

    forenkle mer i continentfilter

--- Oblig/H25/Oblig 2/Oppgave 2/src/components/ContinentFilter.js
+++ Oblig/H25/Oblig 2/Oppgave 2/src/components/ContinentFilter.js
@@ -11,20 +11,20 @@ const ALL_CONTINENTS = [
 export default function ContinentFilter({ selected, onToggle }) {
   return (
     <fieldset className="continent-filter">
-      <div className="checks">
-        {ALL_CONTINENTS.map(function (c) {
-          return (
-            <label key={c} className="check">
-              <input
-                type="checkbox"
-                checked={selected.includes(c)}
-                onChange={function(){ onToggle(c); }}
-              />
-              {c}
-            </label>
-          );
-        })}
-      </div>
+      
+      {ALL_CONTINENTS.map(function (c) {
+        return (
+          <label key={c} className="check">
+            <input
+              type="checkbox"
+              checked={selected.includes(c)}
+              onChange={function(){ onToggle(c); }}
+            />
+            {c}
+          </label>
+        );
+      })}
+ 
     </fieldset>
   );
 }
