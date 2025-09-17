1686f4849eb0044bf009a75435c9b86fda8c4ff6
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-09-17 14:52:39 +0200

    kommenter pagionation

--- Oblig/H25/Oblig 2/Oppgave 2/src/components/Pagination.js
+++ Oblig/H25/Oblig 2/Oppgave 2/src/components/Pagination.js
@@ -1,29 +1,28 @@
 //src/components/Pagination.js
 export default function Pagination({ pager, onPrev, onNext }) {
-  if (!pager) return null;                        // Ingen pager enda ⇒ ikke render noe
-
-  const { currentPage, pageCount } = pager;       // Nåværende side og totalt antall sider
+  if (!pager) return null;
 
   return (
-    <nav className="pagination">
-      {/* Vis bare "Previous" når vi ikke er på første side */}
-      {currentPage > 1 && (
-        <button className="pagination-btn" onClick={onPrev}>
+    <nav className="pagination" aria-label="Pagination">
+      {/* "previous" vises bare når pager ikke er på første side */}
+      {/* Dette skjer når den nåværende siden er mer enn én */}
+      {pager.currentPage > 1 && (
+        <button className="pagination-btn" onClick={onPrev} type="button">
           &lsaquo; Previous
         </button>
       )}
 
-      {/* viser hvilken side bruker er på av totalt antall sider */}
-      <span>Page {currentPage} of {pageCount}</span>
+      {/* Tekst mellom knappene som viser hvilken "page" brukeren er på. */}
+      <span>Page {pager.currentPage} of {pager.pageCount}</span>
 
-      {/* Vis kun "Next" når det faktisk er en neste side. */}
-      {currentPage < pageCount && (
-        <button className="pagination-btn" onClick={onNext}>
-           Next &rsaquo;
+      {/* Viser bare "next" "Next" når det faktisk finnes en neste side */}
+      {/* Dette skjer når den nåværende siden er mindre enn totalt antall pages */}
+      {pager.currentPage < pager.pageCount && (
+        <button className="pagination-btn" onClick={onNext} type="button">
+          Next &rsaquo;
         </button>
       )}
     </nav>
   );
 }
-
 //NB! &rsaquo; og &lsaquo; er høyre og venstre-pil-ikon inne i next- og previousknappene
