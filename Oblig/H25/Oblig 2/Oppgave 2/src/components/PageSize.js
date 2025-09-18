61e388fc8ba74869df2e5256782620e948791f86
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-09-18 19:23:33 +0200

    fjern redundans

--- Oblig/H25/Oblig 2/Oppgave 2/src/components/PageSize.js
+++ Oblig/H25/Oblig 2/Oppgave 2/src/components/PageSize.js
@@ -3,7 +3,7 @@ export default function PageSize({ value, onChange }) {
   return (
     <div className="page-size">
       <label htmlFor="page-size">Results per page:</label>
-      <select id="page-size" value={value} onChange={(e) => onChange(Number(e.target.value))}>
+      <select value={value} onChange={(e) => onChange(Number(e.target.value))}>
         <option value={10}>10</option>
         <option value={20}>20</option>
         <option value={50}>50</option>
