ab300eb4ba74af025990de65f0a057d85a692a45
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-09-20 10:36:56 +0200

    legg inn hardkodet tabell

--- Nettkurs/DIHS2_kurs/tabel/src/components/Browse.jsx
+++ Nettkurs/DIHS2_kurs/tabel/src/components/Browse.jsx
@@ -1,4 +1,49 @@
-//src/comoponents/Browse.jsx
+// src/components/Browse.jsx
+import {
+    Table,
+    TableHead,
+    TableRowHead,
+    TableCellHead,
+    TableBody,
+    TableRow,
+    TableCell,
+    TableFoot,
+} from '@dhis2/ui'
+
 export function Browse() {
-  return <h1>Browse</h1>;
+    // Hardkodede rader
+    const rows = [
+        { id: 'JIazHXNSnFJ', displayName: 'Injectable antibiotics', value: 42 },
+        { id: 'DTtCy7Nx5jH', displayName: 'Total population < 5 years', value: 128 },
+        { id: 'WUg3MYWQ7pt', displayName: 'Total Population', value: 5200 },
+    ]
+
+    return (
+        <Table>
+          
+            <TableHead>
+                <TableRowHead>
+                    <TableCellHead>Display name</TableCellHead>
+                    <TableCellHead>Value</TableCellHead>
+                    <TableCellHead>ID</TableCellHead>
+                </TableRowHead>
+            </TableHead>
+
+            <TableBody>
+                {rows.map(row => (
+                    <TableRow key={row.id}>
+                        <TableCell>{row.displayName}</TableCell>
+                        <TableCell>{row.value}</TableCell>
+                        <TableCell>{row.id}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+
+            <TableFoot>
+                <TableRow>
+                    <TableCell colSpan="3">Hardkodet eksempel</TableCell>
+                </TableRow>
+            </TableFoot>
+        </Table>
+    )
 }
