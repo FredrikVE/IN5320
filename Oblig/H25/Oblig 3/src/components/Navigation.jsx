a3f9f1acee747e4d3c99739d653bdfdfe76288c0
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-09-22 15:17:56 +0200

    kommenter

--- Oblig/H25/Oblig 3/src/components/Navigation.jsx
+++ Oblig/H25/Oblig 3/src/components/Navigation.jsx
@@ -4,24 +4,31 @@ import { TabBar, Tab } from "@dhis2/ui"
 export default function Navigation({ activeTab, onChangeTab, TABS }) {
   return (
     <TabBar>
+
+      {/*Tab for datasets */}
       <Tab
         selected={activeTab === TABS.DATASETS}
         onClick={() => onChangeTab(TABS.DATASETS)}
       >
         Datasets
       </Tab>
+
+      {/*Tab for browse */}
       <Tab
         selected={activeTab === TABS.BROWSE}
         onClick={() => onChangeTab(TABS.BROWSE)}
       >
         Browse
       </Tab>
+
+      {/*Tab for insert */}
       <Tab
         selected={activeTab === TABS.INSERT}
         onClick={() => onChangeTab(TABS.INSERT)}
       >
         Insert
       </Tab>
+      
     </TabBar>
   )
 }
