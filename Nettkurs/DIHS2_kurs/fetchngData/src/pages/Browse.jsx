09cdf1b851f0056501fa99ee8813786ad28b3521
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-09-23 15:30:52 +0200

    legg inn tabell med data

--- Nettkurs/DIHS2_kurs/fetchngData/src/pages/Browse.jsx
+++ Nettkurs/DIHS2_kurs/fetchngData/src/pages/Browse.jsx
@@ -1,5 +1,8 @@
 import { useDataQuery } from '@dhis2/app-runtime'
 import { dataQuery } from '../data/queries'
+import { CircularLoader } from '@dhis2/ui'
+
+import { Table, TableBody, TableCell, TableCellHead, TableFoot, TableHead, TableRow, TableRowHead } from '@dhis2/ui'
 
 function mergeData(data) {
     return data.dataSets.dataSetElements.map(d => {
@@ -19,18 +22,47 @@ function mergeData(data) {
 
 export default function Browse() {
     const { loading, error, data } = useDataQuery(dataQuery)
+    console.log(mergeData)
     if (error) {
         return <span>ERROR: {error.message}</span>
     }
 
     if (loading) {
-        return <span>Loading...</span>
+        return <CircularLoader large />
     }
 
     if (data) {
         let mergedData = mergeData(data)
         console.log(mergedData)
-    }
 
-    return <h1>Browse</h1>
+        return (
+            <Table>
+                
+                {/* Tabele head med overskrifter */}
+                <TableHead>
+                    <TableRowHead>
+                        <TableCellHead>Display Name</TableCellHead>
+                        <TableCellHead>Value</TableCellHead>
+                        <TableCellHead>ID</TableCellHead>
+                    </TableRowHead>
+                </TableHead>
+
+                {/* hovedinnhold i tabellen */}
+                <TableBody>
+                    {mergedData.map(row => {
+                        return (
+
+                            // Hver rad i TableBody
+                            <TableRow key={row.id}>
+                                <TableCell>{row.displayName}</TableCell>
+                                <TableCell>{row.value}</TableCell>
+                                <TableCell>{row.id}</TableCell>
+                            </TableRow>
+                        )
+                    })}
+                </TableBody>
+
+            </Table>
+        );
+    }
 }
