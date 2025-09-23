1532c74a57df3faeb21f1d3cbaeaa2a059876abc
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-09-23 15:37:30 +0200

    legg inn insert.jsx

--- Nettkurs/DIHS2_kurs/fetchngData/src/pages/Insert.jsx
+++ Nettkurs/DIHS2_kurs/fetchngData/src/pages/Insert.jsx
@@ -1,12 +1,63 @@
-import { NoticeBox } from "@dhis2/ui"
+import {
+    ReactFinalForm,
+    InputFieldFF,
+    Button,
+    SingleSelectFieldFF,
+    hasValue,
+    number,
+    composeValidators,
+} from '@dhis2/ui'
 
-// Legger inn en plassholder for Insert-taben bare for å gi litt "feel" for hvordan tab-navigeringen fungerer
-export default function Insert() {
-  return (
-    <div>
-      <h1>Insert</h1>
-      <NoticeBox>Her kan oppgaven utvides med "Insert" slik som i demovideoen.</NoticeBox>
-    </div>
-  
-  );
+export default function Insert(props) {
+    function onSubmit(formInput) {
+        console.log(formInput)
+    }
+
+    return (
+        <div>
+            <ReactFinalForm.Form onSubmit={onSubmit}>
+                {({ handleSubmit }) => (
+                    <form onSubmit={handleSubmit} autoComplete="off">
+                        <ReactFinalForm.Field
+                            component={SingleSelectFieldFF}
+                            name="dataElement"
+                            label="Select field"
+                            initialValue="WUg3MYWQ7pt"
+                            options={[
+                                {
+                                    label: 'Total Population',
+                                    value: 'WUg3MYWQ7pt',
+                                },
+                                {
+                                    label: 'Population of women of child bearing age (WRA)',
+                                    value: 'vg6pdjObxsm',
+                                },
+                                {
+                                    label: 'Total population < 5 years  ',
+                                    value: 'DTtCy7Nx5jH',
+                                },
+                                {
+                                    label: 'Expected pregnancies',
+                                    value: 'h0xKKjijTdI',
+                                },
+                                {
+                                    label: 'Total population < 1 year   ',
+                                    value: 'DTVRnCGamkV',
+                                },
+                            ]}
+                        />
+                        <ReactFinalForm.Field
+                            name="value"
+                            label="Value"
+                            component={InputFieldFF}
+                            validate={composeValidators(hasValue, number)}
+                        />
+                        <Button type="submit" primary>
+                            Submit
+                        </Button>
+                    </form>
+                )}
+            </ReactFinalForm.Form>
+        </div>
+    )
 }
