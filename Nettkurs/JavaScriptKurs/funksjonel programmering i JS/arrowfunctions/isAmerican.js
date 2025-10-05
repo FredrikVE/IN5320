bd5092ec2ea44f427508cf5e3429856f54867c65
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2025-10-05 16:35:44 +0200

    legg inn isAmerican

---
+++ Nettkurs/JavaScriptKurs/funksjonel programmering i JS/arrowfunctions/isAmerican.js
@@ -0,0 +1,18 @@
+import assert from "assert"; // hvis du bruker ES Modules
+// const assert = require("assert"); // hvis du bruker CommonJS
+
+const isAmerican = (inputString) => {
+  if (inputString.toLowerCase().includes("awesome")) {
+    return true;
+  }
+  return false;
+};
+
+//Tester med assert
+assert.strictEqual(isAmerican("This super mediocre thing was awesome!"), true);
+assert.strictEqual(isAmerican("Awesome, let us do that!"), true);
+assert.strictEqual(isAmerican("That sounds aWeSOme!"), true);
+assert.strictEqual(isAmerican("Brilliant!"), false);
+assert.strictEqual(isAmerican("To us — richer and cleverer than everyone else!"), false);
+
+console.log("All tests passed!");
